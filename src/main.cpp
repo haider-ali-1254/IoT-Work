281f21609978e16be5c71261c5dbdbf186c16f76
haider-ali-1254 <haideralintucs1254@gmail.com>
2025-10-13 20:30:45 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,53 @@
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Wire.h>
 
 // ---- OLED setup ----
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDR 0x3C
 
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void drawPrimitives() {
+  display.drawRect(2, 2, 125, 61, SSD1306_WHITE);
+
+}
+
+void drawTextDemo() {
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(25, 32);
+  display.setTextSize(1);
+  display.println("Haider (1254)");
+
+}
 
 void setup() {
   Wire.begin(21, 22); // ESP32 default I2C pins (SDA=21, SCL=22)
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    for (;;); // Loop forever if OLED not found
+    // If it fails, check wiring and address (0x3C/0x3D)
+    for (;;);
   }
 
+
+
+  // Example 2: shapes
   display.clearDisplay();
+  drawPrimitives();
+  display.display();
+  delay(5000);
+
+  // Example 1: text formatting
+  //display.clearDisplay();
+  drawTextDemo();
+  display.display();
+  delay(5000);
+
+  
 }
 
 void loop() {
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(20, 26);
-  display.println("Haider (1254)");
-  display.drawRect(15, 22, 98, 24, SSD1306_WHITE);
-
-  display.display();
-  delay(2000);
+  
 }

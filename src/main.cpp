7d4e6ba687efc1d81fa5851394b151cee21cf339
haider-ali-1254 <haideralintucs1254@gmail.com>
2025-10-13 20:25:54 +0500

    Hometask

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,20 @@ void setup() {
   Wire.begin(21, 22); // ESP32 default I2C pins (SDA=21, SCL=22)
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    // If it fails, check wiring and address (0x3C/0x3D)
-    for (;;)
-      ;
+    for (;;); // Loop forever if OLED not found
   }
 
   display.clearDisplay();
 }
 
 void loop() {
-
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(20, 26);
-  display.println("Haider: 1254");
+  display.println("Haider (1254)");
+  display.drawRect(15, 22, 98, 24, SSD1306_WHITE);
+
   display.display();
   delay(2000);
 }
